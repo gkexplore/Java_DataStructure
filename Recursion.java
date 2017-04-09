2afcb42ab1ac88c3dc6cb7429c417915f3ead1de
Karthik Ganesan <gkexplore@gmail.com>
2017-04-08 19:32:45 -0700

    Recursion
    added reverse a string and palindrome

--- Recursion.java
+++ Recursion.java
@@ -3,48 +3,80 @@ package com.java.datastructure;
 //Recursion - best sample - factorial - https://www.youtube.com/watch?v=PORo1ut9kMs
 //Recursion - best sample - fibonacci - https://www.youtube.com/watch?v=dsmBRUCzS7k
 public class Recursion {
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Recursion recursion = new Recursion();
 		System.out.println("***********************Factorial*************************");
 		recursion.factorial(4);
 		System.out.println("***********************Fibonacci*************************");
 		int i = 0;
-		while(i<8){
+		while (i < 8) {
 			System.out.println(recursion.fibonacci(i));
 			i++;
 		}
+
 		recursion.fibonacci(7);
+
+		System.out.println("***********************Reverse String*************************");
+		// reverse String
 		System.out.println(recursion.reverseString("Karthik"));
+
+		System.out.println("***********************Palindrome*************************");
+		// madam
+		System.out.println(recursion.isPalindrome("madam"));
 	}
-	
-	public int factorial(int n){
-		if(n==1){
+
+	public int factorial(int n) {
+		if (n == 1) {
 			return 1;
-		}else{
-		    int result = n * factorial(n-1);
-		    return result;
+		} else {
+			int result = n * factorial(n - 1);
+			return result;
 		}
 	}
-	
-	public long fibonacci(int n){
-		
-		if(n==0){
+
+	public long fibonacci(int n) {
+
+		if (n == 0) {
 			return 0;
 		}
-		if(n <=2 ){
+		if (n <= 2) {
 			return 1;
 		}
-		
-		long fib = fibonacci(n-1) + fibonacci(n-2);
+
+		long fib = fibonacci(n - 1) + fibonacci(n - 2);
 		return fib;
 	}
-	
-	public String reverseString(String str){
-		if(str.length()==1)
+
+	// substring(arthik)+k//kihtrak
+	// substring(rthik)+a//kihtra
+	// substring(thik)+r//kihtr
+	// substring(hik)+t//kiht
+	// substring(ik)+h//kih
+	// substring(k)+i//ki
+	// k
+	public String reverseString(String str) {
+		if (str.length() == 1)
 			return str;
 		else
-			System.out.println(str.substring(1, str.length())+"+"+str.charAt(0));
-			return reverseString(str.substring(1, str.length()))+str.charAt(0);
+			System.out.println(str.substring(1, str.length()) + "+"
+					+ str.charAt(0));
+		return reverseString(str.substring(1, str.length())) + str.charAt(0);
 	}
+
+	// isPalindrome(madam) = return isPalindrome(ada) = true
+	// isPalidrome(ada)= return isPalindrome(a) = true
+	// isPalindrome(a) = true
+	// true
+
+	public boolean isPalindrome(String str) {
+		int length = str.length();
+		if (length < 2)
+			return true;
+		else
+			System.out.println(str.substring(1, length - 1));
+		return str.charAt(0) != str.charAt(length - 1) ? false
+				: isPalindrome(str.substring(1, length - 1));
+	}
+
 }
